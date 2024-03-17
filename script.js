4a9cd8af16175566a3398686653aa5daf1725ead
Antônio Gonçalves <143822143+Antoniogoncalvesdev@users.noreply.github.com>
2024-03-17 11:54:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,6 @@ const btn = document.querySelector("#btn")
 const inputBox = document.querySelector("#inputBox");
 
 
-//Pegando a data de hoje e mudando tamanho do container
-function dateTodayF(){
-    const today = document.querySelector("#today")
-    const date= new Date();
-    const dateToday = date.getDate();
-    const monthName = date.toLocaleString('pt-BR', { month: 'long' });
-    const dayName = date.toLocaleString('pt-BR', { weekday: 'long' });
-    today.innerHTML = `${dayName}, ${dateToday} ${monthName}`;
-    today.style.display = "flex";
-
-    const container = document.querySelector("#container")
-    container.style.height = "400px"
-}
-
 //Pegando o valor de alguma cidade da api
 async function getCityBr(city){
     const urlBr = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
@@ -29,39 +15,67 @@ async function getCityBr(city){
 //Função para mostrar a cidade, graus etc
 async function showCityBr(){
     try {
-    const inputBox = document.querySelector("#inputBox").value
-    const data = await getCityBr(inputBox)
+        const inputBox = document.querySelector("#inputBox").value
+        const data = await getCityBr(inputBox)
 
-    //determina graus
-    const timeInCity = document.querySelector("#timeInCity")
-    const weather = parseInt(data.main.temp)
-    timeInCity.innerHTML = `${weather}°C`
+        //determina graus
+        const timeInCity = document.querySelector("#timeInCity")
+        const weather = parseInt(data.main.temp)
+        timeInCity.innerHTML = `${weather}°C`
 
-    //icone e descrição
-    const iconImg = document.querySelector("#iconImg")
-    const descrip = document.querySelector("#descrip")
-    descrip.innerHTML = data.weather[0].description;
-    iconImg.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+        //icone e descrição
+        const iconImg = document.querySelector("#iconImg")
+        const descrip = document.querySelector("#descrip")
+        descrip.innerHTML = data.weather[0].description;
+        iconImg.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
 
-    //nome da cidade
-    const nameCity = document.querySelector("#nameCity")
-    nameCity.innerHTML = `<b>${data.name}</b>`
+        //nome da cidade
+        const nameCity = document.querySelector("#nameCity")
+        nameCity.innerHTML = `<b>${data.name}</b>`
 
-    //umidade
-    const humidity = document.querySelector("#humidity")
-    humidity.innerHTML = `Umidade: ${data.main.humidity}%`
+        //umidade
+        const humidity = document.querySelector("#humidity")
+        humidity.innerHTML = `Umidade: ${data.main.humidity}%`
 
-    //consumindo a api das bandeiras
-    const flagContainer = document.querySelector("#flag-container");
-    flagContainer.innerHTML = `<img src="https://flagsapi.com/${data.sys.country}/flat/64.png" alt="Bandeira de ${data.sys.country}" />`;
+        //consumindo a api das bandeiras
+        const flagContainer = document.querySelector("#flag-container");
+        flagContainer.innerHTML = `<img src="https://flagsapi.com/${data.sys.country}/flat/64.png" alt="Bandeira de ${data.sys.country}" />`;
+        
+        //Pegando a data de hoje e mudando tamanho do container
+        function dateTodayF(){
+            try{
+            const today = document.querySelector("#today")
+            const date= new Date();
+            const dateToday = date.getDate();
+            const monthName = date.toLocaleString('pt-BR', { month: 'long' });
+            const dayName = date.toLocaleString('pt-BR', { weekday: 'long' });
+            today.innerHTML = `${dayName}, ${dateToday} ${monthName}`;
+            today.style.display = "flex";
+
+            const container = document.querySelector("#container")
+            container.style.height = "400px"
+            } catch(error){
+                alert("s")
+            }
+        }
+        dateTodayF()
+    
     } catch (error) {
         alert("Digite uma cidade válida!")
-}}
+    }
+};
+
+//ativa butao enter
+document.addEventListener("keypress", async (e) => {
+    if(e.key === "Enter"){
+        const city = inputBox.value;
+        await showCityBr(city)
+    }
+});
 
 btn.addEventListener("click", () => { 
     if(inputBox.value === ""){
         alert("Digite uma cidade válida")
     }
         showCityBr(); 
-        dateTodayF();
 });
